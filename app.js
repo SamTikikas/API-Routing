3cf5a00446160f60272e1be143e2f74267d01237
root <gerasamtikikas@gmail.com>
2023-12-18 10:17:18 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+app.get("/about/", (request, response) => {
+  response.send("About page");
+});
+app.listen(3000, () => {
+  console.log("server is running");
+});
+module.exports = app;
